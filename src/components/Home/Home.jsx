3134623d305147622151301a80961b2ebcd45415
Kangonga <guchu413@gmail.com>
2023-02-12 01:54:24 +0300

    feat: Add links and nest social media icons within

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,24 @@
 import { useContext, useState } from "react"
-import "./Home.scss"
-import { AppContext } from "../../App"
+
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Navigate, useNavigate } from "react-router-dom";
+
+import "./Home.scss"
+import { AppContext } from "../../App"
 
 export default function Home(){
     const {appContext,setAppContext} = useContext(AppContext)
-    const [slideUp, setSlideUp] = useState(false)
-    const handleClick = ()=>{
-        
+    const navigate = useNavigate()
+    const handleClick = (e)=>{
+        // if(e.target.id=="gitIcon"){
+        //     navigate("https://github.com/Kangonga")
+        // }
+        console.log(e?.target.id)
     }
     return(
         <>
-            <main id="Hero" className={slideUp?"slideUp":""}>
+            <main id="Hero" >
                 <section id="Hero__Text-Container">
                     <p className="Hero__Text-intro">Hello, World.</p>
 
@@ -27,8 +33,12 @@ export default function Home(){
                     About me &gt;&gt;
                 </a>
                 <section className="Hero-icons">
-                <GitHubIcon />
-                <LinkedInIcon />
+                <a href="https://github.com/Kangonga">
+                <GitHubIcon id="gitIcon"/>
+                </a>
+                <a href="www.linkedin.com/in/kevin-guchu">
+                <LinkedInIcon id="linkedInIcon"/>
+                </a>
                 </section>
                 
                 </section>
