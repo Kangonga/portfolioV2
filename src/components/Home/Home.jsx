49726c470bb5a7e7963b3da4f96120243be02889
Kangonga <guchu413@gmail.com>
2023-02-12 02:21:36 +0300

    style: complete social media icon styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,13 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Navigate, useNavigate } from "react-router-dom";
 
 import "./Home.scss"
 import { AppContext } from "../../App"
 
 export default function Home(){
     const {appContext,setAppContext} = useContext(AppContext)
-    const navigate = useNavigate()
-    const handleClick = (e)=>{
-        // if(e.target.id=="gitIcon"){
-        //     navigate("https://github.com/Kangonga")
-        // }
-        console.log(e?.target.id)
-    }
     return(
         <>
             <main id="Hero" >
@@ -29,16 +21,16 @@ export default function Home(){
                     </p>
                     <a
                         className="Hero__link-aboutMe" 
-                        href="#About" onClick={()=>{handleClick();setAppContext({...appContext,activeTab:"About"})}}>
+                        href="#About" onClick={()=>{setAppContext({...appContext,activeTab:"About"})}}>
                     About me &gt;&gt;
                 </a>
                 <section className="Hero-icons">
-                <a href="https://github.com/Kangonga">
-                <GitHubIcon id="gitIcon"/>
-                </a>
-                <a href="www.linkedin.com/in/kevin-guchu">
-                <LinkedInIcon id="linkedInIcon"/>
-                </a>
+                    <a href="https://github.com/Kangonga">
+                        <GitHubIcon id="gitIcon" style={{color: "red"}}/>
+                    </a>
+                    <a href="https://linkedin.com/in/kevin-guchu">
+                        <LinkedInIcon id="linkedInIcon" />
+                    </a>
                 </section>
                 
                 </section>
