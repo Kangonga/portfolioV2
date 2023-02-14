2e2c62e40fe057b3ff1a48a2f7dfe4206fc383ba
Kangonga <guchu413@gmail.com>
2023-02-14 15:58:00 +0300

    feat: Add functionality to the contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,36 @@
+import { useContext } from "react"
+import { AppContext } from "../../App"
 import "./Contact.scss"
 
 export default function Contact() {
+    const {visitorData,setVisitorData} = useContext(AppContext)
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        // console.log(visitorData)
+        setVisitorData({
+            name:"",
+            email:"",
+            subject:"",
+            message:"",
+        })
+    }
+    const handleChange = (e)=>{
+        setVisitorData({
+            ...visitorData,
+            [e.target.name]:e.target.value
+        })
+    }
     return(
         <main id="Contact">
             <h1>Contact</h1>
             <h2>I would love to hear from you</h2>
 
-            <form action="" id="Contact-form">
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email"/>
-                <input type="text" placeholder="subject"/>
-                <textarea rows="3" cols="50" type="text" placeholder="message"/>
+            <form id="Contact-form" onSubmit={handleSubmit}>
+                <input name="name" value={visitorData.name}type="text" placeholder="Name"  onChange={handleChange}/>
+                <input name="email" value={visitorData.email}type="email" placeholder="Email" onChange={handleChange}/>
+                <input name="subject" value={visitorData.subject}type="text" placeholder="subject" onChange={handleChange}/>
+                <textarea name="message" value={visitorData.message}rows="3" cols="50" type="text" placeholder="message" onChange={handleChange}/>
                 <button>Submit</button>
             </form>
         </main>
